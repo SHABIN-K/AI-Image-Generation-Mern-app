60fb8c03fd8de86fc3c3e631b0d212aea03c03f6
Shabin-k <helloshabink@gmail.com>
2023-03-24 15:47:06 +0530

    add some files

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormField = () => {
+  return (
+    <div>FormField</div>
+  )
+}
+
+export default FormField
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { FormField, Card, Loader} from '../components';
+
 
 const Home = () => {
+  
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className="font-extrabold text-[#222]">The Community Showcase</h1>
+      </div>
+    </section>
   )
 }
 
