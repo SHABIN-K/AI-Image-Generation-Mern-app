13c48c714a8a2db058dc8dd864e5ae46d6fcd03d
Shabin-k <helloshabink@gmail.com>
2023-03-24 18:19:50 +0530

    design : home page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField,Loader } from "../components";
 
 const CreatePost = () => {
   return (
---
+++ client/src/utils/index.js
@@ -0,0 +1,10 @@
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
