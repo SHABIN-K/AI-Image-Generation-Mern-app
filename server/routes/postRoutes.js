31b61552b231fa71d1e4273b1825d9082a9e57e2
Shabin-k <helloshabink@gmail.com>
2023-03-26 20:53:42 +0530

    fix : api

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -11,8 +11,8 @@ const router = express.Router();
 // Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 //GET ALL POSTS
