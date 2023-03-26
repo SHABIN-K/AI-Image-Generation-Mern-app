62d67f5ed02c9e9ff784f67685d5e6b1469f83ca
Shabin-k <helloshabink@gmail.com>
2023-03-26 15:44:13 +0530

    fix routes and open api

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E");
