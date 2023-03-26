f02760b195e3a3b7844a590bc00591b1cd19b8fe
Shabin-k <helloshabink@gmail.com>
2023-03-26 16:35:25 +0530

    api setup

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,2 +1,34 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.send("DALL E server");
+});
+
+router.route("/").post(async(req, res) => {
+    try {
+        const { prompt } = req.body;
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: "1024x1024",
+          });
+          image_url = response.data.data[0].url;
+    } catch (error) {
+        
+    }
+  });
+
+
+export default router;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
-import { Configuration, OpenAIApi } from "openai";
 
 import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
 const router = express.Router();
+
+export default router;
