1678b3fb3dac47f915b0d59fb2d3571bbcf47a9a
Shabin-k <helloshabink@gmail.com>
2023-03-26 18:12:43 +0530

    dalle setup completed

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,19 +16,24 @@ router.route("/").get((req, res) => {
   res.send("DALL E server");
 });
 
-router.route("/").post(async(req, res) => {
-    try {
-        const { prompt } = req.body;
-        const response = await openai.createImage({
-            prompt: prompt,
-            n: 1,
-            size: "1024x1024",
-          });
-          image_url = response.data.data[0].url;
-    } catch (error) {
-        
-    }
-  });
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image_url = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log("ERROR :", error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
 
 export default router;
