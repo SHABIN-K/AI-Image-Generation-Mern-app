fafebcfe4048facd143422c594015871ecb1f1f3
Shabin-k <helloshabink@gmail.com>
2023-03-26 13:10:35 +0530

    mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
