d7a8eddfb12e9e917d494962582823d1c992c2ae
Shabin-k <helloshabink@gmail.com>
2023-03-29 19:31:26 +0530

    add github corner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 import { logo } from './assets'
 import { Home, CreatePost } from "./pages";
+import GithubCorner from 'react-github-corner';
 
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
         <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
-        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff]  text-white px-4 py-4 rounded-md'>Create</Link>  
+        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff]  text-white px-4 py-4 mr-[50px] rounded-md'>Create</Link>  
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
@@ -20,6 +21,7 @@ const App = () => {
           <Route path='/create-post' element={<CreatePost/>}/>
         </Routes>
       </main>
+      <GithubCorner href="https://github.com/SHABIN-K/AI-Image-Generation-app" />
     </BrowserRouter>
   )
 }
